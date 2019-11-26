505c0566984ced299329c9a95b2c83909eff146d
Stewart Platt <shteou@gmail.com>
2019-11-26 14:59:07 +0000

    Fix directory name in jar copy

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker build . -t quickfix-customisation-builder
 docker run -v `pwd`/quickfix-logging:/quickfix-logging quickfix-customisation-builder
-cp quickfix/target/quickfix-currencycloud-logging-*-SNAPSHOT.jar .
+cp quickfix-logging/target/quickfix-currencycloud-logging-*-SNAPSHOT.jar .
